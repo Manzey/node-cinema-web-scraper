8b0b667f1af83f1b9e3d5d66ba9437c0b14a02c4
David Larsson <manzey@hotmail.se>
2018-01-26 19:02:07 +0100

    Started on exam

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-
+const scrape = require('./lib/URLscraper')
 
 // Check the arguments.
 let args = process.argv.slice(2)
 
 if (args.length === 0) {
-  console.log('ERROR: No argument(s).')
+  console.log('ERROR: No ENTRY URL was provided.')
   process.exit(0)
 }
 
-console.log(args)
+// Scrape the URLs on the argument URL to get access to cinema, etc.
+async function run() {
+const scrapedLinks = scrape.scrapeURLs(args)
+const [linkSet] = await Promise.all([scrapedLinks])
+
+linkSet.forEach(function(link) {
+   console.log(link)
+})
+}
+run()
